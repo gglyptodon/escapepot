5d389f02259fad938cae87c8373b5ccf618e4d15
frogsicle <a7denton@gmail.com>
2017-06-04 21:40:36 +0200

    more mixing buttons (active/innactive division)

--- js/load.js
+++ js/load.js
@@ -6,6 +6,8 @@ var loadState = {
         game.load.image('backgroundredo','assets/img/bg2.png');
         game.load.image('cw', 'assets/img/cw.png');
         game.load.image('ccw', 'assets/img/ccw.png');
+        game.load.image('cw_selec', 'assets/img/cw_selected.png');
+        game.load.image('ccw_selec', 'assets/img/ccw_selected.png');
     },
 
     create: function(){
