7a37cccd84d4ac892ad6c740aedd1ff49116056f
frogsicle <a7denton@gmail.com>
2017-06-04 18:14:57 +0200

    you win! for now

--- js/play.js
+++ js/play.js
@@ -1,1 +1,14 @@
-var playState = {};
+var playState = {
+    create: function(){
+        var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+        placeholdertext = game.add.text(50,50,"four potion ingredients and mixing \nforms coming soon...", style);
+
+        button = game.add.button(game.world.centerX/2, 400, 'button', actionOnClick, this, 2, 1, 0);
+        button.onInputUp.add(this.submit, this);
+    },
+
+    submit: function(){
+        // todo, check status of entered data and decide whether to go to redo or win.
+        game.state.start('win');
+    }
+};
--- js/win.js
+++ js/win.js
@@ -1,1 +1,6 @@
-var winState = {};
+var winState = {
+    create: function(){
+        var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+        placeholdertext = game.add.text(50,50,"Whaaa?? you mixed your\npotion correctly\nsomething exciting happens\n and you're free", style);
+    }
+};
