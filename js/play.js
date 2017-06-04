063d8cdd80bae7bd84c3af48b87cbec8211eb6b5
frogsicle <a7denton@gmail.com>
2017-06-04 20:06:59 +0200

    now with input, and demo validation

--- js/play.js
+++ js/play.js
@@ -7,16 +7,31 @@ var playState = {
 
         button = game.add.button(game.world.centerX/2, 400, 'button', actionOnClick, this, 2, 1, 0);
         button.onInputUp.add(this.submit, this);
+
+        game.add.plugin(PhaserInput.Plugin);
+        input = game.add.inputField(10, 150);
+
     },
 
     submit: function(){
         // todo, check status of entered data and decide whether to go to redo or win.
         var rnum = Math.random();
         console.log(rnum);
-        if (rnum > 0.5){
+        if (this.are_inputs_valid()){
             game.state.start('win');
         }else{
             game.state.start('redo');
+        }
+    },
+
+    are_inputs_valid: function(){
+        var pass_value = "0b08bd98d279b88859b628cd8c061ae0";
+        var hash = CryptoJS.MD5(input.value);
+        console.log(hash.toString(), '-is not-', CryptoJS.MD5("win").toString());
+        if (hash.toString() == pass_value){
+            return(true);
+        }else{
+            return(false);
         }
     }
 };
