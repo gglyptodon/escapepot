85d9a5bd55e398853dbbf684a1606f432e0dfda9
gglyptodon <janina.mass@gmail.com>
2017-06-04 21:47:18 +0200

    direction btn function basics

--- js/play.js
+++ js/play.js
@@ -22,23 +22,33 @@ var playState = {
             var spacing = 90;
             this.ingredientTxt[i] = game.add.inputField(10, spacing*j);
             this.ingredientBtnCW[i] = game.add.button(game.world.centerX/2, spacing*j, 'cw', actionOnClick, this, 2, 1, 0);
+
+            this.ingredientBtnCW[i].onInputUp.add(this.toggleCW.bind(this,i), this);
             this.ingredientBtnCCW[i] = game.add.button(game.world.centerX/2+50, spacing*j, 'ccw', actionOnClick, this, 2, 1, 0);
+
+            this.ingredientBtnCCW[i].onInputUp.add(this.toggleCCW.bind(this,i), this);
+
             this.ingredientDir[i] = 0 ;//todo set to 0 for cw, 1 for ccw
             this.ingredientTimes[i] =  game.add.inputField(350, spacing*j);
-
-
         }
 
+    },
+    toggleCW: function(index){
+    this.ingredientDir[index] = 0;
+    console.log(this.ingredientDir, index);
 
-        //game.add.plugin(PhaserInput.Plugin);
-        //input = game.add.inputField(10, 150);
+    },
+
+    toggleCCW: function(index){
+    this.ingredientDir[index] = 1;
+        console.log(this.ingredientDir, index);
 
     },
 
     submit: function(){
         // todo, check status of entered data and decide whether to go to redo or win.
         var rnum = Math.random();
-        console.log(rnum);
+        //console.log(rnum);
         if (this.are_inputs_valid()){
             game.state.start('win');
         }else{
@@ -48,7 +58,6 @@ var playState = {
 
     are_inputs_valid: function(){
         var isCorrect = true;
-
         var pass_vals =  ["b01b321b66287166bd1552f21a04d8bd", "0b7694d01b81335a241c115c40c033ca", "d39bc8d3d15d9a9000d1ec7c6987eeee", "fb7a1dc716a04f7082894df494e0d0fa"]
          //i1...item4
         var pass_times = [1,2,3,4];
@@ -57,7 +66,6 @@ var playState = {
         for (i = 0; i < this.numInputs; i++) {
             hashed_inputs[i] = CryptoJS.MD5(this.ingredientTxt[i].value+this.ingredientDir[i]+this.ingredientTimes[i].value).toString(); //todo ingrDir to 0 for cw, 1 ccw
             this.resmd5[i] = hashed_inputs[i];
-            console.log(this.resmd5[i]);
             if (hashed_inputs[i].toString() != pass_vals[i]){
                  isCorrect = false
             }
